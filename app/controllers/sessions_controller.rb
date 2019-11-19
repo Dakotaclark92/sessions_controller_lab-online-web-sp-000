606d600b8232a63f4378393eea5e0ecd69fd1348
Dakota Clark <dakotaclark92@yahoo.com>
2019-11-19 03:43:49 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,12 @@ class SessionsController < ApplicationController
   end
   
   def create
-    session[:name] = params[:username]
-    redirect_to '/'
+    if params[:name] == nil || params[:name].empty?
+      redirect_to sessions_new_path
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
   end
   
   def destroy
